17fd0d4b5aaf8a4aa3bbbd00fe51b9f9c95feb6c
Kirill Popov <agrotechnology3000@gmail.com>
2026-03-09 13:22:55 +0300

    Upgrade 3.0

--- src/components/table.js
+++ src/components/table.js
@@ -8,44 +8,46 @@ import {cloneTemplate} from "../lib/utils.js";
  * @returns {{container: Node, elements: *, render: render}}
  */
 export function initTable(settings, onAction) {
-    const { tableTemplate, rowTemplate, before, after } = settings;
+    const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
 
-    before.reverse().forEach((subName) => {
-        root[subName] = cloneTemplate(subName);
-        root.container.prepend(root[subName].container);
+    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach(subName => {
+        root[subName] = cloneTemplate(subName); 
+        root.container.prepend(root[subName].container); 
     });
 
-    after.forEach((subName) => {
-        root[subName] = cloneTemplate(subName);
-        root.container.append(root[subName].container);
+    
+    after.forEach(subName => {
+        root[subName] = cloneTemplate(subName); 
+        root.container.append(root[subName].container); 
     });
 
-    root.container.addEventListener('change', (e) => {
-        onAction(e.target);
+    // @todo: #1.3 —  обработать события и вызвать onAction()
+     root.container.addEventListener('change', () => onAction());
+    root.container.addEventListener('reset', () => {
+    setTimeout(onAction); 
     });
-    root.container.addEventListener('reset', () => setTimeout(onAction, 100));
     root.container.addEventListener('submit', (e) => {
-        e.preventDefault();
-        onAction(e.submitter);
+    e.preventDefault(); 
+    onAction(e.submitter); 
     });
 
     const render = (data) => {
-
-        const nextRows = data.map((item) => {
+        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
+        const nextRows = data.map(item => {
             const row = cloneTemplate(rowTemplate);
-
-            Object.keys(item).forEach((key) => {
-                if (key in row.elements) {
-                    row.elements[key].textContent = item[key];
-                }
-            });
-
+            Object.keys(item).forEach(key => { 
+                if (row.elements[key]) {
+                
+                row.elements[key].textContent = item[key];
+            }
+            })
             return row.container;
-        });
-
+         })
         root.elements.rows.replaceChildren(...nextRows);
-    };
+    }
 
-    return { ...root, render };
+    return {...root, render};
+   
 }
